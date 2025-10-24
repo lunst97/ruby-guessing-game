30e41cb2df5e0028849d405fdbb38cee563a7a52
Git Demo User <demo@example.com>
2025-10-24 11:59:41 +0300

    Fix tests to use correct game files

--- spec/guessing_game_spec.rb
+++ spec/guessing_game_spec.rb
@@ -1,22 +1,27 @@
-require_relative '../guessing_game'
+require_relative '../game'
+require_relative '../player'
+require_relative '../utils'
 
 RSpec.describe 'Guessing Game' do
+  describe 'Game class' do
+    it 'creates a game instance' do
+      expect { Game }.not_to raise_error
+    end
+  end
+
+  describe 'Player class' do
+    it 'creates a player instance' do
+      expect { Player }.not_to raise_error
+    end
+  end
+
   describe 'game logic' do
     it 'generates a random number between 1 and 100' do
       number = rand(1..100)
       expect(number).to be_between(1, 100)
     end
 
-    it 'compares guess with target number' do
-      target = 50
-      expect(30 < target).to be true
-      expect(70 > target).to be true
-      expect(50 == target).to be true
-    end
-  end
-
-  describe 'input validation' do
-    it 'accepts valid numeric input' do
+    it 'validates numeric input' do
       input = "42"
       expect(input.to_i).to be_a(Integer)
       expect(input.to_i).to be > 0
